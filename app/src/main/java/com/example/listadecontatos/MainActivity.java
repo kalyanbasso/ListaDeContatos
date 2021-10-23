83ee7eb210d6c03c3f9601b24f6643325bb87911
Kalyan Basso <kalyan@unochapeco.edu.br>
2021-10-23 14:27:36 -0300

    update 1

--- app/src/main/java/com/example/listadecontatos/MainActivity.java
+++ app/src/main/java/com/example/listadecontatos/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.listadecontatos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText usuario, senha;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = findViewById(R.id.bt_login);
+        usuario = findViewById(R.id.editUsuario);
+        senha = findViewById(R.id.editSenha);
+
+        btnLogin.setOnClickListener(this::handleLogin);
+    }
+
+    private void handleLogin(View view) {
+        String editUsuario = usuario.getText().toString();
+        String editSenha = senha.getText().toString();
+        if(editUsuario.isEmpty() || editSenha.isEmpty()){
+            Snackbar.make(view, "Usuário ou Senha vazio", Snackbar.LENGTH_SHORT).show();
+        }
     }
 }
